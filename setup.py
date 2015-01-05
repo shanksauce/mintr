8e96edc89b26d7459913fa34304f0a6cd2d86879
Ben <ben@blueboatdesign.com>
2015-01-04 21:02:20 -0600

    PyPI shite

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from distutils.core import setup
 import mintr
+import pandoc
+
+#pandoc.core.PANDOC_PATH = '/path/to/pandoc'
+doc = pandoc.Document()
+doc.markdown = open('README.md').read()
 
 setup(
+  author = mintr.__author__,
   name = mintr.__name__,
   packages = [mintr.__name__], 
   version = mintr.__version__,
+  license = mintr.__license__,
   description = 'An API client for Mint.com',
-  author = mintr.__author__,
-  author_email = 'ben@shankware.com',
+  longdescription = doc.rst,
   url = 'https://github.com/shanksauce/mintr',
-  download_url = 'https://github.com/shanksauce/mintr/tarball/0.1',
-  keywords = ['client', 'api', 'mint', 'json'],
-  classifiers = [],
+  keywords = ['client', 'api', 'mint', 'json']
 )
